c1ddd30f03a515a57b08ae46fd50b22519ba4fad
evanhuiedghfjkhbdsu <d13410013@ems.npu.edu.tw>
2026-03-16 01:09:36 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(290, 81);
+            label1.Name = "label1";
+            label1.Size = new Size(154, 23);
+            label1.TabIndex = 0;
+            label1.Text = "請輸入餐點名稱！";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(111, 81);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 30);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(231, 135);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 2;
+            button1.Text = "點餐";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -1,19 +1,30 @@
-private void btnOrder_Click(object sender, EventArgs e)
+namespace WinFormsApp1
 {
-string meal = txtInput.Text.Trim(); // 
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
-if (string.IsNullOrEmpty(meal))
-{
-//
-MessageBox.Show("請輸入餐點名稱！", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-}
-else if (meal == "漢堡")
-{
-// 
-MessageBox.Show("漢堡賣完了，要不要試試薯條？");
-}
-else
-{
-MessageBox.Show("好的，正在為您準備 " + meal + "...");
-}
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+string meal = textBox1.Text.Trim();
+
+            if (meal == "")
+            {
+                MessageBox.Show("請輸入餐點名稱！");
+            }
+            else if (meal == "漢堡")
+            {
+                MessageBox.Show("漢堡賣完了，要不要試試薯條？");
+            }
+            else
+            {
+                MessageBox.Show("好的，正在為您準備 " + meal + "...");
+            }
+
+        }
+    }
 }
